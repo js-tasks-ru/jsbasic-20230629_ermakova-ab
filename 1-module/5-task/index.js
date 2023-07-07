7e2c90b3ef6f5026cd74e76acb91861f19bdde6e
luna100500 <ermakova.ab@gmail.com>
2023-07-07 13:51:10 +0100

    решение дз номер 2

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length <= maxlength) {
+    return str;
+  }
+  else {
+    str = str.slice(0, maxlength-1) + '…';
+    return str;
+  }
 }
+
